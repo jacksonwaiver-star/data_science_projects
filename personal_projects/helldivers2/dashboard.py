8a8fab54b79df56ad745ede233093d49a200324d
Jackson Weaver <jacksonwaiver@gmail.com>
2026-05-12 16:36:47 -0500

    reverted api

--- personal_projects/helldivers2/dashboard.py
+++ personal_projects/helldivers2/dashboard.py
@@ -101,7 +101,7 @@ SESSION_ID = st.session_state.session_id
 
 API_BASE = "https://datascienceprojects-production.up.railway.app"
 
-API_KEY = os.getenv("DEMO_API_KEY")
+API_KEY = os.getenv("API_KEY")
 HEADERS = {
     "X-API-Key": API_KEY,
     "X-Client-Type": "streamlit_dashboard"
